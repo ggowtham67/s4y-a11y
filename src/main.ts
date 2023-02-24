3d3aa0818b6ca59bd40d941d3fe646ac868e0697
Gowtham G <gowtham@sportskeeda.com>
2023-02-24 18:40:35 +0530

    fix: remove duplicate import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-import {context} from '@actions/github'
-
 type FileStatus = 'added' | 'modified' | 'removed' | 'renamed'
 
 async function run(): Promise<void> {
@@ -15,10 +13,10 @@ async function run(): Promise<void> {
     const octokit = github.getOctokit(token)
 
     // Debug log the payload.
-    core.debug(`Payload keys: ${Object.keys(context.payload)}`)
+    core.debug(`Payload keys: ${Object.keys(github.context.payload)}`)
 
     // Get event name.
-    const eventName = context.eventName
+    const eventName = github.context.eventName
 
     // Define the base and head commits to be extracted from the payload.
     let base: string | undefined
@@ -26,16 +24,16 @@ async function run(): Promise<void> {
 
     switch (eventName) {
       case 'pull_request':
-        base = context.payload.pull_request?.base?.sha
-        head = context.payload.pull_request?.head?.sha
+        base = github.context.payload.pull_request?.base?.sha
+        head = github.context.payload.pull_request?.head?.sha
         break
       case 'push':
-        base = context.payload.before
-        head = context.payload.after
+        base = github.context.payload.before
+        head = github.context.payload.after
         break
       default:
         core.setFailed(
-          `This action only supports pull requests and pushes, ${context.eventName} events are not supported. Please submit an issue on this action's GitHub repo if you believe this in correct.`
+          `This action only supports pull requests and pushes, ${github.context.eventName} events are not supported. Please submit an issue on this action's GitHub repo if you believe this in correct.`
         )
     }
 
@@ -46,7 +44,7 @@ async function run(): Promise<void> {
     // Ensure that the base and head properties are set on the payload.
     if (!(base && head)) {
       core.setFailed(
-        `The base and head commits are missing from the payload for this ${context.eventName} event. Please submit an issue on this action's GitHub repo.`
+        `The base and head commits are missing from the payload for this ${github.context.eventName} event. Please submit an issue on this action's GitHub repo.`
       )
 
       // To satisfy TypeScript, even though this is unreachable.
@@ -59,21 +57,21 @@ async function run(): Promise<void> {
     const response = await octokit.rest.repos.compareCommits({
       base,
       head,
-      owner: context.repo.owner,
-      repo: context.repo.repo
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
     })
 
     // Ensure that the request was successful.
     if (response.status !== 200) {
       core.setFailed(
-        `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. Please submit an issue on this action's GitHub repo.`
+        `The GitHub API for comparing the base and head commits for this ${github.context.eventName} event returned ${response.status}, expected 200. Please submit an issue on this action's GitHub repo.`
       )
     }
 
     // Ensure that the head commit is ahead of the base commit.
     if (response.data.status !== 'ahead') {
       core.setFailed(
-        `The head commit for this ${context.eventName} event is not ahead of the base commit. Please submit an issue on this action's GitHub repo.`
+        `The head commit for this ${github.context.eventName} event is not ahead of the base commit. Please submit an issue on this action's GitHub repo.`
       )
     }
 
