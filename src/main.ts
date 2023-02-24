8422a9cad4f0a6768624ea320f013208f48d7ec8
Gowtham G <gowtham@sportskeeda.com>
2023-02-25 02:15:49 +0530

    fix: stringify payload and log

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function run(): Promise<void> {
     const octokit = github.getOctokit(token)
 
     // Debug log the payload.
-    core.info(`Payload ${github.context.payload}`)
+    core.info(`Payload ${JSON.stringify(github.context.payload, undefined, 2)}`)
 
     // Get event name.
     const eventName = github.context.eventName
